d1cb8bc24d1c024aee6ca57ad64a9933d9aabdc0
recoder_99 <roelcastro_99@outlook.ph>
2024-09-12 02:48:10 +0800

    cli program updated
    NOTE:
    requires testing on windows.

--- backend/filepath_parser.py
+++ backend/filepath_parser.py
@@ -9,9 +9,10 @@ class FilepathParser:
     def __init__(self, filepath : str):
         self.filepath = filepath
 
-        if os.name == "nt":
+        if os.name == "nt": #please test this
             self.is_windows = True
             filepath.replace("\\", "/")
+            filepath.replace(" ", "\\ ")
         
         self.tokens = self.filepath.split("/")
     
@@ -28,7 +29,7 @@ class FilepathParser:
         return self.tokens
     
     def get_dir_only(self):
-        return self.filepath[:len(self.filepath) - len(self.get_filename)]
+        return self.filepath[:len(self.filepath) - len(self.get_filename())]
     
 
 

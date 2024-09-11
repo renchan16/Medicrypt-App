f08ab2522749a8880bea633f8eac34552e2ffbdf
magniefique <bandalacharlesandre@gmail.com>
2024-09-11 22:34:52 +0800

    Updates Regarding the Encrypt Page
    - Added AlgoSwitch Component
    - Edited Encrypt.js Page
    - (TEMPORARY) added pages-css for input placeholder values

--- medicrypt/src/pages/Encrypt.js
+++ medicrypt/src/pages/Encrypt.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { MdArrowBackIosNew } from 'react-icons/md'; // Import the back arrow icon
 import { useNavigate } from 'react-router-dom'; // Import useNavigate for navigation
 import '../pages-css/pages.css';
+import AlgoSwitch from '../components/switches/AlgoSwitch';
 
 function Encrypt() {
   const navigate = useNavigate();
@@ -18,7 +19,10 @@ function Encrypt() {
 
       <div className="mt-16">
         <h1 className="text-3xl font-bold text-white">Encrypt a Video</h1>
-        <h2 className="mt-6 text-sm font-normal text-white">Choose an Encryption Algorithm</h2>
+        <h2 className="mt-6 text-xs font-semibold text-white">Choose an Encryption Algorithm</h2>
+        <AlgoSwitch />
+        <input type='text' placeholder='Enter file path or click the button' className='mt-8 w-96 h-12 rounded-lg'></input>
+        <input type='text' placeholder='Enter a password' className='mt-4 w-96 h-12 rounded-lg'></input>
       </div>
     </div>
   );

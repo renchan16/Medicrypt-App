85a86089b6b27275299fc612b60f4d99fca215fd
renchan16 <caritativorenz@gmail.com>
2024-09-17 22:32:21 +0800

    added loaders

--- medicrypt/src/App.js
+++ medicrypt/src/App.js
@@ -6,18 +6,32 @@ import Decrypt from './pages/Decrypt';
 import Encrypt from './pages/Encrypt';
 import Explore from './pages/Explore';
 import HowToUse from './pages/HowToUse';
-import backgroundImage from './assets/background.png'; // Assuming the image is in assets folder
+import backgroundImage from './assets/background.png'; 
+import { ClimbingBoxLoader } from 'react-spinners'; // Import the loader
 
 function App() {
   const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(true); // State for loading spinner
+  const [dots, setDots] = useState(''); // State for trailing dots in "Loading..."
+
+  // Simulate loading text ellipsis effect
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots((prevDots) => (prevDots.length < 3 ? prevDots + '.' : ''));
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
 
   useEffect(() => {
     axios.get('http://127.0.0.1:8000/')
       .then(response => {
         setMessage(response.data.message);
+        setLoading(false); // Stop loading once the data is fetched
       })
       .catch(error => {
         console.error("There was an error!", error);
+        setLoading(false); // Stop loading even if there's an error
       });
   }, []);
 
@@ -31,13 +45,23 @@ function App() {
           backgroundPosition: 'center',
         }}
       >
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/decrypt" element={<Decrypt />} />
-          <Route path="/encrypt" element={<Encrypt />} />
-          <Route path="/explore" element={<Explore />} />
-          <Route path="/howtouse" element={<HowToUse />} />
-        </Routes>
+        {/* Show spinner while loading */}
+        {loading ? (
+          <div className="flex flex-col justify-center items-center h-full">
+            <ClimbingBoxLoader color="#1D1B20" loading={loading} size={15} />
+            <p className="mb-6 text-xl font-bold text-black">
+              Loading{dots}
+            </p>
+          </div>
+        ) : (
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/decrypt" element={<Decrypt />} />
+            <Route path="/encrypt" element={<Encrypt />} />
+            <Route path="/explore" element={<Explore />} />
+            <Route path="/howtouse" element={<HowToUse />} />
+          </Routes>
+        )}
       </div>
     </Router>
   );

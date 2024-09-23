7c9f87a9f7f28fdbb98ae708c427f5f8571e2983
recoder_99 <roelcastro_99@outlook.ph>
2024-09-23 23:35:51 +0800

    differential analysis complete

--- backend/differential.py
+++ backend/differential.py
@@ -29,14 +29,13 @@ class Differential:
 
     def get_uaci(self, frame1, frame2, width, height):
         temp1 = 1 / (width * height)
-        temp2 = 0
-
+        
         uaci_list = []
         for c in range(3):
+            temp2 = 0
             for i in range(width - 1):
                 for j in range(height - 1):
-                    temp2 += (abs(frame1 - frame2) / 255) * 100
-
+                    temp2 += (abs(frame1[j][i][c].astype(np.float128) - frame2[j][i][c].astype(np.float128)) / 255) * 100
             uaci_list.append(temp1 * temp2)
 
         return uaci_list
@@ -70,4 +69,4 @@ class Differential:
         frame_width = len(e_frame1[0])
         frame_height = len(e_frame1)
 
-        return self.get_npcr(e_frame1, e_frame2, frame_width, frame_height)
+        return self.get_uaci(e_frame1, e_frame2, frame_width, frame_height)

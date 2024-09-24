1f88b9897b36e77d09fdeb6c5f5bdcfe74050a71
jp-jp-123 <jpbeltran098@gmail.com>
2024-09-24 23:20:51 +0800

    Force change to str from Path obj
    cv2 is picky, doesn't recognize filepath object

--- backend/_3d_cosine.py
+++ backend/_3d_cosine.py
@@ -352,12 +352,12 @@ class Encrypt_cosine:
         key_file = open(key_dest.absolute(), "a")
 
         # Prepare the video writer
-        cap = cv2.VideoCapture(fpath.resolve(), cv2.CAP_FFMPEG)
+        cap = cv2.VideoCapture(str(fpath.resolve()), cv2.CAP_FFMPEG)
 
         frame_width = int(cap.get(3))
         frame_height = int(cap.get(4))
         result = cv2.VideoWriter(
-            vid_dest.absolute(),
+            str(vid_dest.absolute()),
             cv2.VideoWriter_fourcc(*"HFYU"),
             cap.get(cv2.CAP_PROP_FPS),
             (frame_height, frame_width),    # we use height, width as the final encryption is rotated 90 degrees

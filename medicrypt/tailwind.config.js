b496ccf56c00aed5a4df599337f6bf4f74844971
renchan16 <caritativorenz@gmail.com>
2024-10-12 20:51:51 +0800

    new ui

--- medicrypt/tailwind.config.js
+++ medicrypt/tailwind.config.js
@@ -6,18 +6,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#0A1F44', // Dark navy blue for trust and professionalism
-        primary0: '#102A55', // Slightly lighter blue for backgrounds or accents
-        primary1: '#0E7490', // Medium cyan-blue for active elements
-        primary2: '#1C2D3E', // Dark grayish blue for deeper accents
-        primary3: '#162337', // Even darker blue for shadows or highlights
-        secondary: '#00CC88', // Bright green for success indicators (secure, encrypted)
-        secondary1: '#99FFC2', // Light green for subtle accents
-        black: '#131517', // Very dark gray for text or icons
-        white: '#F0F4F8', // Off-white for contrast with dark elements
+        primary: '#1e63dc', //encrypt
+        primary0: '#1644b3', //text in Explore
+        primary1: '#1583fe', 
+        primary2: '#1645b4', 
+        primary3: '#1644b3', //Instructions Blue
+        secondary: '#102a6b', 
+        secondary1: '#103d8b', 
+        black: '#1d1d1f', 
+        white: '#F0F4F8', 
+        background:'#1583fe',
       },
       fontSize: {
-        'xxs': '0.75rem', // Slightly smaller for very subtle text
+        'xxs': '0.60rem', 
       },
     },
   },

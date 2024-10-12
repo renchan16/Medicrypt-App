3b315efe54fc05692dc97adc0fe49c1800c5195a
renchan16 <caritativorenz@gmail.com>
2024-10-12 20:51:51 +0800

    new ui

--- medicrypt/src/pages/LandingPage.js
+++ medicrypt/src/pages/LandingPage.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
-import { HiArrowRight } from 'react-icons/hi';
+import { FaChevronCircleRight } from "react-icons/fa";
 import Card from '../components/Cards';
 import { useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import '../pages-css/LandingPage.css';
 import logo from '../assets/MedicryptLogo.png';
+import InstructionsGif from '../assets/InstructionsAnimation.gif';
+import LearnMoreAnimation from '../assets/LearnMoreAnimation.gif';
+import DecryptAnimation from '../assets/DecryptAnimation.gif';
+import EncryptAnimation from '../assets/EncryptAnimation.gif';
 
 function LandingPage() {
   const navigate = useNavigate();
@@ -15,94 +19,133 @@ function LandingPage() {
     setTimeout(() => navigate(path), 300);
   };
 
-  // Animation variants for motion
-  const fadeIn = {
-    hidden: { opacity: 0, scale: 0.9 },
-    visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
-    exit: { opacity: 0, scale: 0.9, transition: { duration: 0.3 } },
+  // Pan animation variants for motion
+  const panVariants = {
+    hidden: { x: '-100vw', opacity: 0 },
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: { type: 'spring', duration: 2, bounce: 0.5 },
+    },
+    exit: { x: '100vw', opacity: 0, transition: { duration: 0.5 } },
   };
 
   return (
-    <motion.div
-      className="flex flex-col justify-center items-center min-h-screen"
-      initial="hidden"
-      animate="visible"
-      exit="exit"
-      variants={fadeIn}
-    >
-      <div className="flex items-center mb-7">
-        <img src={logo} alt="Medicrypt Logo" className="w-20 mt-2" />
-        <h1 className="text-7xl font-bold text-transparent bg-clip-text gradient-text leading-tight pb-2">medicrypt.</h1>
-      </div>
-
+    // Ensure the outermost div has a full screen background
+    <div className="min-h-screen landing-background flex justify-center items-center">
       <motion.div
-        className="sibling-fade grid grid-cols-3 grid-rows-2 gap-x-3 gap-y-1"
+        className="flex flex-col justify-center items-center min-h-screen"
         initial="hidden"
         animate="visible"
         exit="exit"
-        variants={fadeIn}
+        variants={panVariants}
       >
-        <Card width="col-span-1 row-span-5" height="h-[350px]">
-          <motion.div
-            className="bg-primary h-full flex flex-col justify-between p-3 rounded-[18px] relative transition-all duration-300 hover:bg-[#3a72bb] hover:shadow-lg hover:scale-105"
-            whileHover={{ scale: 1.05 }}
-            onClick={() => handleNavigate('/howtouse')}
-          >
-            <div className="flex-grow flex items-end">
-              <p className="text-white font-bold text-xl transition-colors duration-300 hover:text-gray-200">Instructions</p>
-            </div>
-            <HiArrowRight className="text-white font-[10px] absolute bottom-4 right-4 transition-transform duration-300 transform hover:translate-x-2" />
-          </motion.div>
-        </Card>
+        <div className="flex items-center mb-7">
+          <img src={logo} alt="Medicrypt Logo" className="w-20 mt-2" />
+          <h1 className="text-7xl font-bold text-white bg-clip-text leading-tight pb-1">medicrypt</h1>
+        </div>
 
-        <Card width="col-span-2 row-span-3" height="h-[120px]">
-          <motion.div
-            className="section-transparent border-2 border-black h-full flex flex-col justify-between p-3 rounded-[18px] relative transition-all duration-300 hover:border-[#000000] hover:bg-gray-100 hover:shadow-lg hover:scale-105"
-            whileHover={{ scale: 1.05 }}
-            onClick={() => handleNavigate('/explore')}
-          >
-            <div className="flex-grow flex items-end">
-              <p className="text-black font-bold text-xl transition-colors duration-300">Explore Features</p>
-            </div>
-            <HiArrowRight className="text-black font-semibold absolute bottom-4 right-4 transition-transform duration-300 transform hover:translate-x-2" />
-          </motion.div>
-        </Card>
+        <motion.div
+          className="sibling-fade grid grid-cols-3 grid-rows-2 gap-x-3 gap-y-1"
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+          variants={panVariants}
+        >
+          {/* Instructions Card */}
+          <Card width="col-span-1 row-span-5" height="h-[350px]">
+            <motion.div
+              className="bg-primary3 h-full flex flex-col justify-between p-3 rounded-[18px] relative transition-all duration-300 hover:bg-secondary hover:shadow-lg hover:scale-105 z-10"
+              whileHover={{ scale: 1.05 }}
+              onClick={() => handleNavigate('/howtouse')}
+            >
+              <div className="flex-grow flex justify-center items-center">
+                <div className="w-50 h-40 mt-20 mb-5 overflow-hidden z-10">
+                  <img src={InstructionsGif} alt="Instructions Animation" className="w-full h-40 object-contain" />
+                </div>
+              </div>
+              <div className="flex-grow flex items-end">
+                <p className="text-white font-bold text-2xl transition-colors duration-300 hover:text-gray-200">Instructions</p>
+              </div>
+              <div className="inline-block">
+                <button className='bg-white text-primary3 text-xxs rounded-[10px] w-15 px-2 py-0.5 font-bold mt-1'>HOW TO USE THE APP</button>
+              </div>
+              <FaChevronCircleRight className="text-white text-3xl absolute bottom-4 right-4 transition-transform duration-300 transform hover:translate-x-2" />
+            </motion.div>
+          </Card>
 
-        <Card width="col-span-1 row-span-2" height="h-[190px]">
-          <motion.div
-            className="bg-primary1 h-full flex flex-col justify-between p-3 rounded-[18px] relative transition-all duration-300 hover:bg-[#101e31] hover:shadow-lg hover:scale-105"
-            whileHover={{ scale: 1.05 }}
-            onClick={() => handleNavigate('/encrypt')}
-          >
-            <div className="flex-grow flex items-end">
-              <p className="text-white font-bold text-xl transition-colors duration-300 hover:text-gray-200">Encrypt</p>
-            </div>
-            <div>
-              <p className="text-white text-xxs">Secure your video files</p>
-            </div>
-            <HiArrowRight className="text-white font-semibold absolute bottom-4 right-4 transition-transform duration-300 transform hover:translate-x-2" />
-          </motion.div>
-        </Card>
+          {/* Explore Features Card */}
+          <Card width="col-span-2 row-span-3" height="h-[120px]">
+            <motion.div
+              className="bg-white h-full flex justify-between items-center p-3 rounded-[18px] relative transition-all duration-300 hover:border-primary hover:bg-gray-100 hover:shadow-lg hover:scale-105 z-10"
+              whileHover={{ scale: 1.05 }}
+              onClick={() => handleNavigate('/explore')}
+            >
+              <div className="flex flex-col justify-center items-start">
+                <p className="text-primary0 font-bold text-2xl transition-colors duration-300">
+                  Explore Features
+                </p>
+                <button className="bg-primary3 text-white text-xxs rounded-[10px] w-15 px-2 py-0.5 font-bold mt-1">
+                  LEARN MORE
+                </button>
+              </div>
+              <div className="w-40 h-30">
+                <img
+                  src={LearnMoreAnimation}
+                  alt="Learn More Animation"
+                  className="w-full h-full object-contain"
+                />
+              </div>
+              <FaChevronCircleRight className="text-primary0 text-3xl font-semibold absolute bottom-4 right-4 transition-transform duration-300 transform hover:translate-x-2" />
+            </motion.div>
+          </Card>
 
-        <Card width="col-span-1 row-span-2" height="h-[190px]">
-          <motion.div
-            className="bg-black h-full flex flex-col justify-between p-3 rounded-[18px] relative transition-all duration-300 hover:bg-[#0d0d0e] hover:shadow-lg hover:scale-105"
-            whileHover={{ scale: 1.05 }}
-            onClick={() => handleNavigate('/decrypt')}
-          >
-            <div className="flex-grow flex items-end">
-              <p className="text-white font-bold text-xl transition-colors duration-300 hover:text-gray-200">Decrypt</p>
-            </div>
-            <div>
-              <p className="text-white text-xxs">Access your video files</p>
-            </div>
-            <HiArrowRight className="text-white font-semibold absolute bottom-4 right-4 transition-transform duration-300 transform hover:translate-x-2" />
-          </motion.div>
-        </Card>
+          {/* Encrypt Card */}
+          <Card width="col-span-1 row-span-2" height="h-[190px]">
+            <motion.div
+              className="bg-primary h-full flex flex-col justify-between p-3 rounded-[18px] relative transition-all duration-300 hover:bg-secondary1 hover:shadow-lg hover:scale-105 z-10"
+              whileHover={{ scale: 1.05 }}
+              onClick={() => handleNavigate('/encrypt')}
+            >
+              <div className="flex-grow flex justify-center items-center mb-8">
+                <div className="w-40 h-20 z-10">
+                  <img src={EncryptAnimation} alt="Encrypt Animation" className="w-full h-29 object-scale-down" />
+                </div>
+              </div>
+              <div className="flex-grow flex items-end">
+                <p className="text-white font-bold text-2xl transition-colors duration-300 hover:text-gray-200">Encrypt</p>
+              </div>
+              <div className="inline-block">
+                <button className='bg-white text-primary1 text-xxs rounded-[10px] w-15 px-2 py-0.5 font-bold mt-1 '>SECURE YOUR VIDEOS</button>
+              </div>
+              <FaChevronCircleRight className="text-white text-3xl font-semibold absolute bottom-4 right-4 transition-transform duration-300 transform hover:translate-x-2" />
+            </motion.div>
+          </Card>
 
-        
+          {/* Decrypt Card */}
+          <Card width="col-span-1 row-span-2" height="h-[190px]">
+            <motion.div
+              className="bg-black h-full flex flex-col justify-between p-3 rounded-[18px] relative transition-all duration-300 hover:bg-[#0f0f0f] hover:shadow-lg hover:scale-105 z-10"
+              whileHover={{ scale: 1.05 }}
+              onClick={() => handleNavigate('/decrypt')}
+            >
+              <div className="flex-grow flex justify-center items-center mb-2">
+                <div className="w-40 h-20 z-10">
+                  <img src={DecryptAnimation} alt="Decrypt Animation" className="w-full h-30 mb-3 object-fill" />
+                </div>
+              </div>
+              <div className="items-end">
+                <p className="text-white font-bold text-2xl transition-colors duration-300 hover:text-gray-200">Decrypt</p>
+              </div>
+              <div className="inline-block">
+                <button className='bg-white text-black text-xxs rounded-[10px] w-15 px-2 py-0.5 font-bold mt-1'>ACCESS YOUR VIDEOS</button>
+              </div>
+              <FaChevronCircleRight className="text-white text-3xl font-semibold absolute bottom-4 right-4 transition-transform duration-300 transform hover:translate-x-2" />
+            </motion.div>
+          </Card>
+        </motion.div>
       </motion.div>
-    </motion.div>
+    </div>
   );
 }
 

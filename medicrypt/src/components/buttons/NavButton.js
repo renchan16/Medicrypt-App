0558f7479e727cdda4addb8d503509036ac6ce39
renchan16 <caritativorenz@gmail.com>
2024-10-14 21:48:33 +0800

    loading bar

--- medicrypt/src/components/buttons/NavButton.js
+++ medicrypt/src/components/buttons/NavButton.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default function NavButton({ className, buttonText, buttonColor, hoverColor, buttonTextColor, buttonIcon: Icon, onClickFunction, filePath }) {
-    
+export default function NavButton({ className, buttonText, buttonColor, hoverColor, buttonTextColor, hoverTextColor, buttonIcon: Icon, onClickFunction, filePath }) {
+
     if (hoverColor === "") {
         hoverColor = buttonColor;
     }
-    
+
     const handleClick = async () => {
         if (filePath) {
             try {
@@ -15,7 +15,7 @@ export default function NavButton({ className, buttonText, buttonColor, hoverCol
                 console.error("Error opening file location:", error);
             }
         }
-        
+
         if (typeof onClickFunction === 'function') {
             onClickFunction();
         }
@@ -24,7 +24,7 @@ export default function NavButton({ className, buttonText, buttonColor, hoverCol
     return (
         <div className={`${className}`}>
             <button
-                className={`w-full h-full rounded-xl bg-${buttonColor} font-medium text-${buttonTextColor} text-lg flex items-center justify-center transition-colors hover:bg-${hoverColor}`}
+                className={`w-full h-full rounded-md bg-${buttonColor} font-medium text-${buttonTextColor} text-lg flex items-center justify-center transition-colors hover:bg-${hoverColor} hover:text-${hoverTextColor}`}
                 onClick={handleClick}
                 >
                 {Icon && <Icon className="mr-2" size={22} />}

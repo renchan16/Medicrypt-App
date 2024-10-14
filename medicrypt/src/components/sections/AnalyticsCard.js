c89a0188535ab8469357190573a4ce83e501f300
magniefique <bandalacharlesandre@gmail.com>
2024-10-14 17:53:27 +0800

    Added Descriptions except for CC

--- medicrypt/src/components/sections/AnalyticsCard.js
+++ medicrypt/src/components/sections/AnalyticsCard.js
@@ -1,13 +1,20 @@
 import React from "react";
 
-export const AnalyticsCard = ({className, children}) => {
-
+export const AnalyticsCard = ({ className, children, onClick }) => {
     return (
-        <div className={`${className} relative overflow-hidden p-6 shadow-[0_0_4px_1px_rgba(0,0,0,0.10)] rounded-xl`}>
+        <div
+            className={`${className} relative overflow-hidden p-6 shadow-[0_0_4px_1px_rgba(0,0,0,0.10)] rounded-xl ${onClick ? "cursor-pointer" : "cursor-auto"}`}
+            onClick={onClick}
+        >
             {children}
         </div>
     );
 };
 
-export const AnalyticsCardTitle = ({className="", children}) => <h1 className={`${className === "" ? "font-semibold mb-4" : className}`}>{children}</h1>
-export const AnalyticsCardContent = ({className="", children}) => <p className={`${className === "" ? "font-normal" : className}`}>{children}</p>
+export const AnalyticsCardTitle = ({ className = "", children }) => (
+    <div className={`${className === "" ? "font-semibold mb-4" : className}`}>{children}</div>
+);
+
+export const AnalyticsCardContent = ({ className = "", children }) => (
+    <div className={`${className === "" ? "font-normal" : className}`}>{children}</div>
+);
--- medicrypt/src/components/sections/AnalyticsMetrics.js
+++ medicrypt/src/components/sections/AnalyticsMetrics.js
@@ -1,16 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
 import { AnalyticsCard, AnalyticsCardTitle, AnalyticsCardContent } from "./AnalyticsCard";
 import { AnalyticsTooltip } from "./AnalysisTooltip";
 
 const metricFullNames = {
-    "Entropy": "Frame Entropy",
-    "UACI": "Unified Average Changing Intensity",
-    "NPCR": "Number of Pixels Change Rate",
-    "PSNR": "Peak Signal-to-Noise Ratio",
-    "MSE": "Mean Squared Error"
+    "UACI": "(Unified Average Changing Intensity)",
+    "NPCR": "(Number of Pixels Change Rate)",
+    "PSNR": "(Peak Signal-to-Noise Ratio)",
+    "MSE": "(Mean Squared Error)"
+};
+
+const metricDescriptions = {
+    "Entropy": "Metric that measures the randomness of pixel values in a video frame",
+    "UACI": "Metric that measures the resistance to differential attacks.",
+    "NPCR": "Metric that measures the resistance to differential attacks.",
+    "PSNR": "Metric that measures the quality of decrypted frame.",
+    "MSE": "Metric that measures the average squared difference between decrypted and original frames.",
+    "Encryption Time": "Time taken to encrypt the image.",
+    "Decryption Time": "Time taken to decrypt the image."
 };
 
 export const AnalyticsMetrics = ({metric, baselineSpeed, value}) => {
+    const [showDescription, setShowDescription] = useState(false);
+
     const calculatePercentage = () => {
         switch (metric.name) {
             case "Entropy":
@@ -55,22 +66,31 @@ export const AnalyticsMetrics = ({metric, baselineSpeed, value}) => {
     })();
 
     return (
-        <AnalyticsCard>
-            <AnalyticsTooltip 
-                metric={metricFullNames[metric.name] || metric.name} 
-                >
-                <AnalyticsCardTitle>
+        <AnalyticsCard
+            className={"h-48"}
+            onClick={() => setShowDescription(!showDescription)}>
+            <AnalyticsTooltip metric={metricFullNames[metric.name] || metric.name}>
+                <AnalyticsCardTitle
+                    className="font-semibold mb-0">
                     {metric.name}
                 </AnalyticsCardTitle>
             </AnalyticsTooltip>
-            <AnalyticsCardContent>
-                <h1 className="text-4xl font-bold mb-2">
-                    {metric.name === "Entropy" ? value[0].toFixed(4) : value.toFixed(4)}
-                    {metric.name === "NPCR" || metric.name === "UACI" ? "%" : ""}
-                    {metric.name === "Encryption Time" || metric.name === "Decryption Time" ? "s" : ""}    
-                </h1>
-                <p className="text-sm mt-2">Ideal: {metric.ideal}</p>
-            </AnalyticsCardContent>
+            {showDescription === true ? 
+            <div>
+                <AnalyticsCardContent>
+                    <div className="text-sm mt-4 text-justify">{metricDescriptions[metric.name]}</div>
+                </AnalyticsCardContent>
+            </div> : 
+            <div>
+                <AnalyticsCardContent>
+                    <div className="text-4xl font-bold mt-6 mb-2">
+                        {metric.name === "Entropy" ? value[0].toFixed(4) : value.toFixed(4)}
+                        {metric.name === "NPCR" || metric.name === "UACI" ? "%" : ""}
+                        {metric.name === "Encryption Time" || metric.name === "Decryption Time" ? "s" : ""}    
+                    </div>
+                    <div className="text-sm mt-4">Ideal: {metric.ideal}</div>
+                </AnalyticsCardContent>
+            </div>}
             <div 
                 className={`absolute bottom-0 left-0 h-1 transition-all duration-500 ${isGood ? 'bg-green-500' : 'bg-red-500'}`} 
                 style={{ width: `${percentage}%` }}

7abb9bb317da43950313a5dd53c98740650b36b4
jp-jp-123 <jpbeltran098@gmail.com>
2024-11-08 12:42:08 +0800

    Header and function comments

--- backend/utils/key_validator.py
+++ backend/utils/key_validator.py
@@ -1,4 +1,32 @@
+"""
+The key_validator.py contains the script for validation of the key file before being used by the algorithm.
+This avoids errors and unexpected behaviors of the program when a wrong key file is used
+
+Functionality:
+--------------
+1. Type Safety:
+    - Uses enums to be more type safe, and allows the code authors to avoid running into a typo
+
+2. Exceptions
+    - Raises error when invalid key file is used, halts the scripts from running.
+    This error is received by the api and the frontend will display the appropriate error
+
+Functions:
+1. __checkNumLiteral__(sample: str):
+    - Tests whether the obtained example key from the file can be turned into float
+    - True means it is a key for 3D_Cosine
+    - False means it is a key for Fisher-Yates
+
+2.  def validateKey(sample: str, mode: EncryptionMode):
+    - Tests whether the key is approriate for the selected EncryptionMode
+    - Raises an error if incompatible
+
+Code Author: John Paul M. Beltran
+"""
+
+
 from enum import Enum, auto
+
 
 class EncryptionMode(Enum):
     FISHER_YATES = auto()
